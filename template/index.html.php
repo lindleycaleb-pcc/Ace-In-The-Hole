9a97723f647ffd16b7065c1578d288aa25cdd361
lindleycaleb-pcc <64160893+lindleycaleb-pcc@users.noreply.github.com>
2020-05-08 13:54:37 -0700

    Add files via upload

---
+++ template/index.html.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ace In The Hole - Template</title>
+    <meta name="" content="">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css" type="text/css">
+    <link href="styles/helper.css" rel="stylesheet" type="text/css">
+    <link href="styles/grid.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <header>
+    <div id="wrapper">
+        <?php include 'includes/header.inc.html.php'; ?>
+        <?php include 'includes/nav.inc.html.php'; ?>
+    </div>
+        <div class="hero-image"></div>
+    </header>
+
+    <main id="container">
+
+    <section id="top" class="content">
+
+    </section>
+
+    <section id="second-section" class="content">
+
+    </section>
+
+    <section id="third-section" class="content">
+
+    </section>
+
+    <section id="fourth-section" class="content">
+
+    </section>
+
+    <section id="fifth-section" class="content">
+
+    </section>
+
+
+    <section id="right-bottom" class="content">
+        <p>Right-Bottom (Desktop Version)</p>
+    </section>
+
+    <section id="left-bottom" class="content">
+        <p>Left-Bottom (Desktop Version)</p>
+
+    </section>
+
+        <?php include 'includes/footer.inc.html.php'; ?>
+
+</main>
+</body>
+
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script  src="scripts/scripts.js"></script>
+
+</html>
