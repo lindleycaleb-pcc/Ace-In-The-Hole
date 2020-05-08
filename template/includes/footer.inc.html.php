3cd196e750618625b51b3a7ba9c04ea39cf48e68
lindleycaleb-pcc <64160893+lindleycaleb-pcc@users.noreply.github.com>
2020-05-08 13:52:40 -0700

    Upload Template Files

---
+++ template/includes/footer.inc.html.php
@@ -0,0 +1,16 @@
+<footer>
+    <div class="brand">
+        <a href="<?php echo BASE_URL; ?>index.html.php">
+            <img src="<?php echo BASE_URL . ('images/LOGO.png'); ?>"
+                 alt="Ace In The Hole Multisport Events Logo" width="200px"></a>
+    </div>
+    <div class="footernav">
+        <a href="<?php echo BASE_URL; ?>/contact/contact.html.php">Contact</a>
+        <a href="<?php echo BASE_URL; ?>/faq/faq.html.php">FAQs</a>
+    </div>
+
+    <div id="copyright">
+    <p>&copy; 2020 Ace In The Hole Multisport Events<p>
+    </div>
+
+</footer>
---
+++ template/registration/athlete-registration.html.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ace In The Hole - Athlete Registration - Template</title>
+    <meta name="" content="">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href=../styles/style.css>
+</head>
+
+<body>
+    <header>
+    <div id="wrapper">
+        <?php include '../includes/header.inc.html.php'; ?>
+        <?php include '../includes/nav.inc.html.php'; ?>
+    </div>
+        <div class="hero-image"></div>
+    </header>
+
+    <main>
+    <section class="content">
+        <h1>Heading 1</h1>
+
+    </section>
+
+    <section class="content">
+        <h2>Athlete Registration From</h2>
+    </section>
+
+    <section class="content">
+
+    </section>
+
+    <?php include '../includes/footer.inc.html.php'; ?>
+
+</main>
+</body>
+
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script  src="../scripts/scripts.js"></script>
+
+</html>
