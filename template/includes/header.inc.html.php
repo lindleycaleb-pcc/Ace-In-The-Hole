1fa2b8ac49c0de5e92a1d5caa588a774663e02b4
lindleycaleb-pcc <64160893+lindleycaleb-pcc@users.noreply.github.com>
2020-05-08 13:48:08 -0700

    Delete header.inc.html.php

--- template/includes/header.inc.html.php
+++
@@ -1,8 +0,0 @@
-<?php define('BASE_URL', '/cas222/template/'); ?>
-<header>
-    <div class="brand">
-        <a href="<?php echo BASE_URL; ?>index.html.php">
-            <img src="<?php echo BASE_URL . ('images/AITH_LOGO.png'); ?>"
-                 alt="Ace In The Hole Multisport Events Logo" width="200px"></a>
-    </div>
-</header>
