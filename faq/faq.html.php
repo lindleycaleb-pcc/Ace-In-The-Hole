ae388d557022f0226afeb45adbbf0ee03277ade5
lindleycaleb-pcc <64160893+lindleycaleb-pcc@users.noreply.github.com>
2020-05-31 14:32:42 -0700

    Add files via upload

--- faq/faq.html.php
+++ faq/faq.html.php
@@ -19,28 +19,52 @@
     </header>
 
     <main>
-    <section class="content">
-        <h1>Heading 1</h1>
+    <section class="faqs-content width-1200">
+        <h1>FAQs</h1>
+        <div class="dropdown7">
+            <button onclick="myFunction7()" class="dropbtn7"><strong>What are the rules?</strong><span>+</span></button>
+            <div id="myDropdown7" class="dropdown-content7">
+                <p>We currently adhere to the USAT Rules for Triathlon & Duathlon. Important
+                    rules include no drafting, you must wear a helmet and music is ABSOLUTELY
+                    NOT allowed during the bike for obvious safety reasons.</p>
+            </div>
+        </div>
+        <div class="dropdown8">
+            <button onclick="myFunction8()" class="dropbtn8"><strong>Can I use a personal music device<br> while cycling?</strong><span>+</span></button>
+            <div id="myDropdown8" class="dropdown-content8">
+                <p>Absolutely no music devices are allowed during the bike segment for obvious
+                    safety risks and will result in immediate disqualification.</p>
+            </div>
+        </div>
+        <div class="dropdown9">
+            <button onclick="myFunction9()" class="dropbtn9"><strong>Can I use a personal music device<br> while running?</strong><span>+</span></button>
+            <div id="myDropdown9" class="dropdown-content9">
+                <p>During any running segments, we prefer for athletes to NOT use music devices to maximize safety, assure a fair competitive environment and assure athletes can hear all course marshal instructions. With that said, we also understand that many athletes rely on music to help endure the challenge of running. To accommodate the needs of our athletes while still assuring maximum safety and a fair, competitive environment, here is the arrangement that we can accommodate.</p>
+                <ul>
+                    <li>If you chose to use a personal music device, you must always keep one ear open for instructions so only one ear-bud can be used at any time. In addition, if you opt to use music, you will not be eligible for awards, points and rankings. So as an athlete, you have to make a choice - music or awards/points/rankings.</li>
+                    <li>If you are using a personal music devise with two earbuds, you will be immediately disqualified. Trust us, we don't want to have to enforce a disqualification but in order to assure the safety of all athletes, this is a very important rule.</li>
+                    <li>We also request that if you chose to use a personal music device that you keep the volume to a minimum to assure all instructions can be heard.</li>
+                </ul>
+            </div>
+        </div>
+        <div class="dropdown10">
+            <button onclick="myFunction10()" class="dropbtn10"><strong>Do I need to wear a wetsuit?</strong><span>+</span></button>
+            <div id="myDropdown10" class="dropdown-content10">
+                <p>No, you do not need to wear a wetsuit. Many will not wear a wetsuit while others will choose to wear a wetsuit because of the buoyancy and warmth factor.</p>
+            </div>
+        </div>
+        <div class="dropdown11">
+            <button onclick="myFunction11()" class="dropbtn11"><strong>Do I have to use a road or racing<br> bike?</strong><span>+</span></button>
+            <div id="myDropdown11" class="dropdown-content11">
+                <p>No. We welcome any type of bike as long as it is functioning properly with brakes and endcaps at the end of your handlebars.</p>
+            </div>
+        </div>
 
     </section>
 
-    <section class="content">
-        <h2>FAQ</h2>
-    </section>
-
-    <section class="content">
-        <h2>FAQ</h2>
-    </section>
-
-    <section class="content">
-         <h2>FAQ</h2>
-    </section>
-
-    <section class="content">
-         <h2>FAQ</h2>
-    </section>
-
+    <div id="faq-footer">
     <?php include '../includes/footer.inc.html.php'; ?>
+    </div>
 
 </main>
 </body>
